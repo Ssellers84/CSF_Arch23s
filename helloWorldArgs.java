a12e005b515733e41a59f5046bb5ebcdb471b267
Sean <seansellers84@gmail.com>
2023-04-12 18:05:52 -0700

    init

---
+++ helloWorldArgs.java
@@ -0,0 +1,8 @@
+public class helloWorldArgs {
+    public static void main(String[]args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("Number of command line args: " + args.length);
+    }
+}
+
