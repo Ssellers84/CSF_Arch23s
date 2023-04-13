ad724e5b64cc38f8fc69309c451bd3671aa4c7b6
Sean <seansellers84@gmail.com>
2023-04-12 17:11:14 -0700

    import math library and print sqrt(4)

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,10 @@
+import java.lang.Math;
 public class helloWorld
 {
     public static void main(String[]args)
     {
         System.out.println("Hello World!");
 
+        System.out.println("sqrt(4) is " + Math.sqrt(4));
     }
 }
