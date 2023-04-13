e76454aecde3da75a7d0593c7927a496a73add0f
Sean <seansellers84@gmail.com>
2023-04-12 17:17:28 -0700

    add print of sqrt (4)

--- helloWorld.c
+++ helloWorld.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <math.h>
 
 int main()
 {
-    printf("Hello World!");
-   
+    printf("Hello World!\n");
+    printf("sqrt(4) is %f\n", sqrt(4));
+
     return 0;
 }
 
